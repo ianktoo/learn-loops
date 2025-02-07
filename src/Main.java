c626cb482588e0c43bd4ced3e91268c03115afaf
ianktoo <ianktoo@gmail.com>
2025-02-06 20:47:57 -0800

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        // For loop
+        for(int counter = 1; counter <= 5; counter++){
+            System.out.println("Counter: " + counter);
+        }
+
+        for(double i = 2.0; i <= 5.0; i++){
+            System.out.println("Calculated "+ i +" interest rate of 10,000 = " + calculateInterest(10_000, i));
+        }
+
+        for(double rate = 7.5; rate <= 15.0; rate += 0.25){
+            System.out.println("Calculated "+ rate +" interest rate of 10,000 = " + calculateInterest(10_000, rate));
+        }
+    }
+
+    // Method to calculate an interest amount.
+    public static double calculateInterest(double amount, double rate){
+        return (amount * (rate/100));
+    }
+
+
 }
